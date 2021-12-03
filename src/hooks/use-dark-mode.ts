7a5038d9a3d8084a5a23e9e3d9f00b8178dba89c
delbertbeta <delbertbeta@live.com>
2021-12-03 23:51:20 +0800

    feat: init this repo with vite

---
+++ src/hooks/use-dark-mode.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+export const useDarkMode = () => {
+  useEffect(() => {
+    const mql = window.matchMedia('(prefers-color-scheme: dark)');
+
+    function matchMode(e: MediaQueryListEvent) {
+      const body = document.body;
+      if (e.matches) {
+        if (!body.hasAttribute('theme-mode')) {
+          body.setAttribute('theme-mode', 'dark');
+        }
+      } else {
+        if (body.hasAttribute('theme-mode')) {
+          body.removeAttribute('theme-mode');
+        }
+      }
+    }
+
+    mql.addEventListener('change', matchMode);
+
+    return () => {
+      mql.removeEventListener('change', matchMode);
+    };
+  }, []);
+};
